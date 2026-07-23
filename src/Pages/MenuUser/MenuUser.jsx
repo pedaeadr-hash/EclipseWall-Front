24e890e402621d6f2f48936dea77f850c012e6bc
Adrian Ariel <pedaeadr@gmail.com>
2026-07-22 23:37:54 -0300

    New : fetch uploadWall

--- src/Pages/MenuUser/MenuUser.jsx
+++ src/Pages/MenuUser/MenuUser.jsx
@@ -8,8 +8,11 @@ import logo from '../../assets/loginto.png'
 import verificarzzz from "../../assets/veri.png"
 
 export default function Menu (){
+    const [mensagemwall,setmensagemwall] = useState("")
     const [ativarmsg,setativarmsg] = useState(false)
-    
+    const [nomewall,setnomewall] = useState ('undefined');
+    const [urlwall,seturlwall] = useState ('undefined');
+    const [categoriawall,setcategoriawall] = useState ('undefined');
     const[ fotoperfil,setfotoperfil] = useState ("https://i.pinimg.com/736x/d7/b9/48/d7b948ff970f7d92ee265072da06fd07.jpg")
     const [QueryCategoria, setQueryCategoria ] =useState("Default")
     const [listauniquecategoria,setlistauniquecategoria] = useState([])
@@ -149,6 +152,27 @@ export default function Menu (){
                 console.log(error)
             }
         }
+        
+
+        const uploadwall = async ()=>{
+            try {
+                
+                if (nomewall == 'undefined' || urlwall == 'undefined' || categoriawall == 'undefined' || nomewall === '' || urlwall === '' || categoriawall === '') {
+            return setmensagemwall("Campos vazios");
+                }
+
+                const response = await fetch("http://localhost:5115/api/WallEndPoints/UpWallpaper",{
+                    method:"POST",
+                    headers:{"Content-Type": "application/json"},
+                    body:JSON.stringify({Nome:nomewall,Url:urlwall,categoria:categoriawall})
+                })
+                if (response.ok){
+                    setmensagemwall("Upload realizado com sucesso")
+                }
+            } catch (error){
+                console.log(error);
+            }
+        }
 
     useEffect(()=>{
         verificar();
@@ -179,7 +203,11 @@ export default function Menu (){
                         <span className="contentclose">
                             <svg onClick={()=>{setupload(false)}} className="close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
                         </span>
-
+                            <input type="text" placeholder="NOME" onChange={(e)=>{setnomewall(e.target.value)}}/>
+                            <input type="text" placeholder="URL" onChange={(e)=>{seturlwall(e.target.value)}}/>
+                            <input type="text" placeholder="CATEGORIA" onChange={(e)=>{setcategoriawall(e.target.value)}}/>
+                            <p>{mensagemwall}</p>
+                            <button onClick={ ()=>{uploadwall()}}>enviar</button>
                     </div>
                 </div>
                 <div className={`mensagemicon ${ativarmsg ? "activemsgicon": ""}`}>
@@ -191,7 +219,6 @@ export default function Menu (){
                 <div className={`EditIconConteiner ${EditIcon ? "iconative" : ""}`}>
                     <span className="contentclose">
                             <svg onClick={()=>{setEditIcon(!EditIcon)}} className="close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
-                
                     </span>
                     <div className="gird">
                         <div className="iconsselect" onClick={()=>{setfotoperfil("https://i.pinimg.com/1200x/29/dc/83/29dc83410cc93ca8676b36024b0b10d1.jpg")}}><img src="https://i.pinimg.com/1200x/29/dc/83/29dc83410cc93ca8676b36024b0b10d1.jpg" alt="" /></div>
