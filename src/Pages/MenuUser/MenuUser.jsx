b9af1c402e81cd4d266becc0572152d1ba9f1da7
Adrian Ariel <pedaeadr@gmail.com>
2026-07-27 23:25:15 -0300

    CREAT : DESIGNE UPLOAD FOR ADMIN

--- src/Pages/MenuUser/MenuUser.jsx
+++ src/Pages/MenuUser/MenuUser.jsx
@@ -203,11 +203,14 @@ export default function Menu (){
                         <span className="contentclose">
                             <svg onClick={()=>{setupload(false)}} className="close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
                         </span>
+                            <h1>UPLOAD WALLPAPER</h1>
+                            <div className="upcontent">
                             <input type="text" placeholder="NOME" onChange={(e)=>{setnomewall(e.target.value)}}/>
                             <input type="text" placeholder="URL" onChange={(e)=>{seturlwall(e.target.value)}}/>
                             <input type="text" placeholder="CATEGORIA" onChange={(e)=>{setcategoriawall(e.target.value)}}/>
-                            <p>{mensagemwall}</p>
-                            <button onClick={ ()=>{uploadwall()}}>enviar</button>
+                            </div>
+                            <p className="ufap">{mensagemwall}</p>
+                            <button className="bbbup" onClick={ ()=>{uploadwall()}}>enviar</button>
                     </div>
                 </div>
                 <div className={`mensagemicon ${ativarmsg ? "activemsgicon": ""}`}>
