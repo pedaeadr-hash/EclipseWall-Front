8790f086ddb056b32cfc131f61a4f6ea07a581c9
Adrian Ariel <pedaeadr@gmail.com>
2026-07-30 23:50:08 -0300

    NEW : PageForDownload

--- src/Pages/Download/DownloadWall.jsx
+++ src/Pages/Download/DownloadWall.jsx
@@ -1,9 +1,16 @@
+import "../../Css/Download.css"
 import { useParams } from "react-router-dom"
 export default function Download (){
     const {slug} = useParams()
     return(
-        <div>
-            <h1>oi aq {slug}</h1>
+        <div className="download">
+            <img className="fundodown" src="https://images.hdqwalls.com/wallpapers/bthumb/batman-is-not-a-replacement-he-is-a-revolution-4i.jpg" alt="" />
+            <img className="downloadimg" src="https://images.hdqwalls.com/wallpapers/bthumb/batman-is-not-a-replacement-he-is-a-revolution-4i.jpg" alt="" />
+            <p className="pforwall">Batman Is Not A Replacement He Is A Revolution</p>
+            <div className="contentdown">
+                <div className="down"><p>Donwloads (0)</p></div>
+                <button>Instalar</button>
+            </div>
         </div>
     )
 }
